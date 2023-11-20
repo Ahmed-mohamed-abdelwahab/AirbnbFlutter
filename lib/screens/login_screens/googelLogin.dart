ab9157b0450417dec10a292779202eb31b8e28db
your_github_username <your_github_email>
2023-11-20 09:40:57 +0200

    add firbase

---
+++ lib/screens/login_screens/googelLogin.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginGoogel extends StatelessWidget {
+  const LoginGoogel({super.key});
+
+  // Obtain the auth details from the request
+  Future<UserCredential> signInWithGoogle() async {
+    // Create a new provider
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    googleProvider
+        .addScope('https://www.googleapis.com/auth/contacts.readonly');
+    googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+    // Or use signInWithRedirect
+    // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all()),
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: MaterialButton(
+          onPressed: () {
+            signInWithGoogle();
+            
+          },
+          child: Row(children: [
+            Expanded(
+              child: SizedBox(
+                child: Center(
+                  child: Text("المتابعة باستخدام Google"),
+                ),
+              ),
+            ),
+            Icon(Icons.directions_subway_filled_outlined),
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login_screens/loginscreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lec2/screens/login_emaibas/logi_screen.dart';
+import 'package:lec2/screens/login_screens/appleLogin.dart';
+import 'package:lec2/screens/login_screens/emailLogin.dart';
+import 'package:lec2/screens/login_screens/faceBookLogin.dart';
+import 'package:lec2/screens/login_screens/googelLogin.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: const [Icon(Icons.close)],
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Text("تسجيل الدخول /الاشتراك في Airbnb"),
+            // LoginFaceBook(),
+            LoginScreen1(),
+            const LoginEmail(),
+            const LoginGoogel(),
+            const LoginApple(),
+           const LoginFaceBook ()
+          ],
+        ),
+      ),
+    );
+  }
+}
